b6f6eef3f343a5a25848ef6e3d99b2b3945c5769
Anand Kumar Sinha <109061880+AnandKumarSinha07@users.noreply.github.com>
2024-01-26 21:47:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,68 @@
-const gameInfo = document.querySelector('.game-info');
-const boxes = document.querySelectorAll('.box');
-const newGameBtn = document.querySelector('.new-game-btn');
-
-// decide the current player
-let currentPlayer;
-
-// All possible combinations to win
-const winningPositions = [
-    // horizontal
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    // vertical
-    [0, 3, 6],
-    [1, 4, 7], 
-    [2, 5, 8], 
-    // diagonal
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-let gameGrid;
+const input=document.getElementById('input-box');
+const listContainer=document.getElementById('list-container')
+const listBox=document.getElementById('checked');
+const button=document.getElementById('btn');
 
 
-// Initialize the game
-function initGame(){
-    currentPlayer = 'x';
-    gameGrid = ["", "", "", "", "", "", "", "", ""];
-
-    boxes.forEach((box, index) => {
-        box.innerText = "";
-        box.style.pointerEvents = "all";
-        // box.classList = `box box-${index + 1}`;
-        box.classList.remove("win");
-    });
-
-    gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
-    newGameBtn.classList.remove("active");
-}
-
-initGame();
-
-
-// event listener for each box
-boxes.forEach((box, index) => {
-    box.addEventListener('click', () => {
-        handleClick(index);
-    });
-});
-
-function handleClick(index){
-    if(gameGrid[index] === ""){
-        boxes[index].innerText = currentPlayer.toUpperCase();
-        boxes[index].style.pointerEvents = "none";
-        gameGrid[index] = currentPlayer;
-        // swapping player's turn
-        swapTurn();
-        // checking game is over or not
-        checkGameOver();
+function addTask(){
+    if(input.value==='')
+    {
+        alert('Enter Something')
     }
-}
-
-// swap player turns
-function swapTurn(){
-    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
-    gameInfo.innerText = `Current Player - ${currentPlayer.toUpperCase()}`;
-}
-
-function checkGameOver(){
-    let winner = "";
-    // checking if any player matches the winning combinations
-    winningPositions.forEach((position) => {
-        if(gameGrid[position[0]] != "" && 
-            gameGrid[position[1]] != "" && 
-            gameGrid[position[2]] != "" && 
-            gameGrid[position[0]] === gameGrid[position[1]] &&
-            gameGrid[position[1]] === gameGrid[position[2]]
-        ){
-            boxes.forEach((box) => {
-                box.style.pointerEvents = "none";
-            });
-
-            winner = gameGrid[position[0]] === 'x' ? 'x' : 'o'; 
-            boxes[position[0]].classList.add("win");
-            boxes[position[1]].classList.add("win");
-            boxes[position[2]].classList.add("win");
-        }
-    });
-
-    // checks if we get the winner
-    if(winner != ""){
-        gameInfo.innerText = `Winner - ${winner.toUpperCase()}`;
-        newGameBtn.classList.add("active");
-        return;
+    else{
+        const li=document.createElement('li');
+        li.innerHTML=input.value;
+        listContainer.appendChild(li);
+        input.value='';
+        const span=document.createElement('span');
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        
     }
-
-    // checks if its a draw
-    let allBoxesFilled = true;
-    gameGrid.forEach((box) => {
-        if(box === ""){
-            allBoxesFilled = false;
-        }
-    });
-
-    if(allBoxesFilled){
-        gameInfo.innerText = `It's a Draw`;
-        newGameBtn.classList.add("active");
-    }
+    SavedData();
 }
 
-newGameBtn.addEventListener('click', initGame);
+listContainer.addEventListener('click',(e)=>{
+      if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked")
+      }
+      else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove()
+      }
+},false)
+
+function SavedData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showData() {
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
+showData();
+
+
+/*-------------Geolocatio ----------------------------------*/
+function gotLocation(position){
+      console.log(position);
+}
+
+function failedLocation(){
+    alert('not able to get Location')
+}
+button.addEventListener('click',async()=>{
+    const result=navigator.geolocation.getCurrentPosition(gotLocation,failedLocation)
+})
+/*-------------------------Time---------------------------------*/
+function showTime(){
+    const currenTime=new Date();
+    const timeRead=`${currenTime.getHours()}:${currenTime.getMinutes()}:${currenTime.getSeconds()}`;
+
+    console.log(currenTime);
+    console.log(timeRead);
+    const display=document.getElementById('clock');
+    display.innerHTML=timeRead;
+    display.style.display="block"
+}
+showTime();
+// setInterval(showTime,1000);
